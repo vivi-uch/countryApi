5ce84314dbaf9041ff655627fc9c56467793c0d8
Okonkwo Vivian <vivianokonkwouch@gmail.com>
2026-03-02 14:39:23 +0100

    fixed resolution problem

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,10 @@ const Header = ({
   darkMode: boolean;
 }) => {
   return (
-    <div className="py-6 px-2 sm:p-4 flex justify-between items-center border-b border-gray-300 bg-slate-100 dark:bg-[#1c2226]">
-      <h1 className="text-[13.5px] sm:text-xl font-bold">Where in the world?</h1>
+    <div className="py-4 px-4 md:px-12 flex justify-between items-center border-b border-gray-300 bg-slate-100 dark:bg-[#1c2226]">
+      <h1 className="text-[13.5px] sm:text-xl font-bold">
+        Where in the world?
+      </h1>
       <button
         className="text-[13px] sm:text-md flex items-center gap-1 sm:gap-2 text-black dark:text-white p-0 sm:px-4 sm:py-2 rounded-md"
         onClick={toggleDarkMode}
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -6,15 +6,15 @@ type LayoutProps = {
   darkMode: boolean;
 };
 
-const Layout = ({toggleDarkMode, darkMode}: LayoutProps) => {
-return (
+const Layout = ({ toggleDarkMode, darkMode }: LayoutProps) => {
+  return (
     <div className=" min-h-screen text-black  bg-slate-50 dark:bg-[#1a1e31] dark:text-white">
-        <Header toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
-        <div className="p-6  lg:p-0  mx-auto mt-2 sm:mt-12 container">
-            <Outlet/>
-        </div>
+      <Header toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+      <div className="mx-auto px-4 sm:px-12 mt-6 sm:mt-12">
+        <Outlet />
+      </div>
     </div>
-);
+  );
 };
 
 export default Layout;
